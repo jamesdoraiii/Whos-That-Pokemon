582db04a4d55915a68c71ec472fcbe39f658e6ff
James <jamesdoraiii@gmail.com>
2019-03-19 13:55:38 -0400

    added https

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const baseURL = "http://pokeapi.salestock.net/api/v2/pokemon/";
+const baseURL = "https://pokeapi.salestock.net/api/v2/pokemon/";
 
 //UI setup
 const playbutton = document.getElementById("play");
